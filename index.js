667aa096899189d03ddfaaff01d3d9fa6170f3a2
venkateswarareddyvaka <venkatehvaka81@gmail.com>
2023-02-16 10:03:17 +0530

    modification done

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const add=require('add');
+const math=require("mathjs")
 
 let array=[1,3,5,7,9];
 
 console.log(add(array))
+
+const sub=require('add');
+
+let array1=[1,3,5,7,9];
+
+console.log(math.sum(array1))
